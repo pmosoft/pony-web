e2cf9f6f1c223e65b3f47a039aa9c95a370c3859
P147970 <P147970@SKTP147970PN001.SKT.AD>
2019-09-17 14:07:27 +0900

    ..

--- src/app/layout/etl/etl-tab/etl-tab.component.ts
+++ src/app/layout/etl/etl-tab/etl-tab.component.ts
@@ -20,15 +20,15 @@ export class EtlTabComponent implements OnInit {
  ***************************************************/
   comm : Comm = new Comm();
 
-  tabInfoInVo: TabInfo = new TabInfo();
-  tabInfoOutVoList: TabInfo[]
-  jdbcInfoInVo: JdbcInfo = new JdbcInfo();
+  srcTabInfoInVo: TabInfo = new TabInfo();
+  tarTabInfoInVo: TabInfo = new TabInfo();
+  srcTabInfoOutVoList: TabInfo[]
+  tarTabInfoOutVoList: TabInfo[]
+  srcJdbcInfoInVo: JdbcInfo = new JdbcInfo();
+  tarJdbcInfoInVo: JdbcInfo = new JdbcInfo();
 
-  createScript = "";
-  tabRowsUpdateScript = "";
-  
-  //comboJdbc : JdbcCombo[];
-  comboJdbc : JdbcInfo[];
+  comboSrcJdbc : JdbcInfo[];
+  comboTarJdbc : JdbcInfo[];
   
   comboWhereTabs = [
 	    {name : 'T0' , value : 'T0' }
@@ -43,19 +43,10 @@ export class EtlTabComponent implements OnInit {
 	   ,{name : 'TAB' , value : 'TAB' }
 	  ];  
   
-  comboOrderBy = [
-      {name : '선택(정렬)' , value : 'JDBC_NM' }
-     ,{name : '테이블명'   , value : 'TAB_NM'  }
-     ,{name : '테이블한글명', value : 'TAB_HNM'  }
-     ,{name : 'Rows'     , value : 'TAB_ROWS'}
-     ,{name : '최근변경일' , value : 'TAB_UPD_DT'}
-     ,{name : '테이블생성일', value : 'TAB_REG_DT'}
-  ];
   
-  cnt = 0;
+  srcCnt = 0;
+  tarCnt = 0;
   
-  comboAscDesc = this.tabInfoService.comboAscDesc;
-
   isCheckAll : boolean = false;
 
   constructor(private tabInfoService: TabInfoService
@@ -68,27 +59,40 @@ export class EtlTabComponent implements OnInit {
  ***************************************************/
   ngOnInit() {
     //this.tabInfoInVo = this.tabInfoService.onGetLocalStorageTabInfo();
-    this.onSetComboJdbc();
-    this.onSelectEtlTab();
+    this.onSetComboSrcJdbc();
+    this.onSetComboTarJdbc();
+    //this.onSelectSrcTabList();
     
-    this.tabInfoInVo.whereTabs1 = this.comm.nullToSpace(localStorage.getItem('whereTabs1'));
-    this.tabInfoInVo.whereTabs2 = this.comm.nullToSpace(localStorage.getItem('whereTabs2'));
-    this.tabInfoInVo.whereTabs3 = this.comm.nullToSpace(localStorage.getItem('whereTabs3'));
-    this.tabInfoInVo.whereTabs4 = this.comm.nullToSpace(localStorage.getItem('whereTabs4'));
+    this.srcTabInfoInVo.whereTabs1 = this.comm.nullToSpace(localStorage.getItem('whereTabs1'));
+    this.srcTabInfoInVo.whereTabs2 = this.comm.nullToSpace(localStorage.getItem('whereTabs2'));
+    this.srcTabInfoInVo.whereTabs3 = this.comm.nullToSpace(localStorage.getItem('whereTabs3'));
+    this.srcTabInfoInVo.whereTabs4 = this.comm.nullToSpace(localStorage.getItem('whereTabs4'));
     
   }
 
   /***************************
    * JDBC콤보 박스 초기 세팅
    **************************/
-  onSetComboJdbc() {
-    this.jdbcInfoService.selectComboJdbcList(this.jdbcInfoInVo)
+  onSetComboSrcJdbc() {
+    this.jdbcInfoService.selectComboJdbcList(this.srcJdbcInfoInVo)
     //this.jdbcInfoService.selectJdbcInfoList(this.jdbcInfoInVo)
     .subscribe(result => {
       if(!result.isSuccess) alert(result.errUsrMsg)
       else {
         //console.log(result.jdbcInfoOutVoList);
-        this.comboJdbc = result.jdbcInfoOutVoList;
+        this.comboSrcJdbc = result.jdbcInfoOutVoList;
+      }
+    });
+  }
+
+  onSetComboTarJdbc() {
+    this.jdbcInfoService.selectComboJdbcList(this.tarJdbcInfoInVo)
+    //this.jdbcInfoService.selectJdbcInfoList(this.jdbcInfoInVo)
+    .subscribe(result => {
+      if(!result.isSuccess) alert(result.errUsrMsg)
+      else {
+        //console.log(result.jdbcInfoOutVoList);
+        this.comboTarJdbc = result.jdbcInfoOutVoList;
       }
     });
   }
@@ -100,25 +104,27 @@ export class EtlTabComponent implements OnInit {
   /********************
    * JDBC변경시
    ********************/
-  onChangeComboJdbc(i) {
-    //console.log("event.value=="+event.value);
-    //console.log("event.value=="+event.selectedIndex);
-    //console.log("event.value=="+event.options[event.selectedIndex]);
-    //console.log("event.value=="+event.options[event.selectedIndex].owner);
-    //var aa = event.options[event.selectedIndex];
-    //console.log("event.value=="+aa.owner);
-    //console.log("event.value=="+this.comboJdbc[0].usrId);
-    //console.log(usrId);
-    //console.log("aa".toUpperCase());
+  onChangeComboSrcJdbc(i) {
     if(i==0) {
-      this.tabInfoInVo.jdbcNm = ""
-      this.tabInfoInVo.owner = ""
+      this.srcTabInfoInVo.jdbcNm = ""
+      this.srcTabInfoInVo.owner = ""
     } else {
-      this.tabInfoInVo.jdbcNm = this.comboJdbc[i].jdbcNm;
-      this.tabInfoInVo.owner = this.comboJdbc[i].usrId;
+      this.srcTabInfoInVo.jdbcNm = this.comboSrcJdbc[i].jdbcNm;
+      this.srcTabInfoInVo.owner = this.comboSrcJdbc[i].usrId;
     }
-    this.onSelectEtlTab();
+    this.onSelectSrcTabList();
   }
+
+  onChangeComboTarJdbc(i) {
+    if(i==0) {
+      this.tarTabInfoInVo.jdbcNm = ""
+      this.tarTabInfoInVo.owner = ""
+    } else {
+      this.tarTabInfoInVo.jdbcNm = this.comboTarJdbc[i].jdbcNm;
+      this.tarTabInfoInVo.owner = this.comboTarJdbc[i].usrId;
+    }
+  }
+
 
   /*****************************
    * IN 테이블 검색
@@ -127,66 +133,51 @@ export class EtlTabComponent implements OnInit {
     //localStorage.setItem('ponyTabs1' , this.tabInfoInVo.whereTabs);
     console.log(i);
     console.log(this.comboWhereTabs[i].value);
-    this.tabInfoInVo.chkWhereTabs = (i==0) ? false : true;
-    this.tabInfoInVo.selectedTabs = i;
-    //this.tabInfoInVo.whereTabs = this.comboWhereTabs[i-1].value;
-    this.tabInfoInVo.tabNm = "";
-    if(this.tabInfoInVo.selectedTabs==0) this.tabInfoInVo.whereTabs = "";
-    if(this.tabInfoInVo.selectedTabs==1) this.tabInfoInVo.whereTabs = this.tabInfoInVo.whereTabs1;
-    if(this.tabInfoInVo.selectedTabs==2) this.tabInfoInVo.whereTabs = this.tabInfoInVo.whereTabs2;
-    if(this.tabInfoInVo.selectedTabs==3) this.tabInfoInVo.whereTabs = this.tabInfoInVo.whereTabs3;
-    if(this.tabInfoInVo.selectedTabs==4) this.tabInfoInVo.whereTabs = this.tabInfoInVo.whereTabs4;
+    this.srcTabInfoInVo.chkWhereTabs = (i==0) ? false : true;
+    this.srcTabInfoInVo.selectedTabs = i;
+    //this.srcTabInfoInVo.whereTabs = this.comboWhereTabs[i-1].value;
+    this.srcTabInfoInVo.tabNm = "";
+    if(this.srcTabInfoInVo.selectedTabs==0) this.srcTabInfoInVo.whereTabs = "";
+    if(this.srcTabInfoInVo.selectedTabs==1) this.srcTabInfoInVo.whereTabs = this.srcTabInfoInVo.whereTabs1;
+    if(this.srcTabInfoInVo.selectedTabs==2) this.srcTabInfoInVo.whereTabs = this.srcTabInfoInVo.whereTabs2;
+    if(this.srcTabInfoInVo.selectedTabs==3) this.srcTabInfoInVo.whereTabs = this.srcTabInfoInVo.whereTabs3;
+    if(this.srcTabInfoInVo.selectedTabs==4) this.srcTabInfoInVo.whereTabs = this.srcTabInfoInVo.whereTabs4;
 
   }
   onChangeTabs(i: number) {
-    if(this.tabInfoInVo.selectedTabs==0) this.tabInfoInVo.whereTabs = "";
-    if(this.tabInfoInVo.selectedTabs==1) this.tabInfoInVo.whereTabs = this.tabInfoInVo.whereTabs1;
-    if(this.tabInfoInVo.selectedTabs==2) this.tabInfoInVo.whereTabs = this.tabInfoInVo.whereTabs2;
-    if(this.tabInfoInVo.selectedTabs==3) this.tabInfoInVo.whereTabs = this.tabInfoInVo.whereTabs3;
-    if(this.tabInfoInVo.selectedTabs==4) this.tabInfoInVo.whereTabs = this.tabInfoInVo.whereTabs4;
+    if(this.srcTabInfoInVo.selectedTabs==0) this.srcTabInfoInVo.whereTabs = "";
+    if(this.srcTabInfoInVo.selectedTabs==1) this.srcTabInfoInVo.whereTabs = this.srcTabInfoInVo.whereTabs1;
+    if(this.srcTabInfoInVo.selectedTabs==2) this.srcTabInfoInVo.whereTabs = this.srcTabInfoInVo.whereTabs2;
+    if(this.srcTabInfoInVo.selectedTabs==3) this.srcTabInfoInVo.whereTabs = this.srcTabInfoInVo.whereTabs3;
+    if(this.srcTabInfoInVo.selectedTabs==4) this.srcTabInfoInVo.whereTabs = this.srcTabInfoInVo.whereTabs4;
 
-    if(i==1) localStorage.setItem('whereTabs1',this.tabInfoInVo.whereTabs1);
-    if(i==2) localStorage.setItem('whereTabs2',this.tabInfoInVo.whereTabs2);
-    if(i==3) localStorage.setItem('whereTabs3',this.tabInfoInVo.whereTabs3);
-    if(i==4) localStorage.setItem('whereTabs4',this.tabInfoInVo.whereTabs4);
+    if(i==1) localStorage.setItem('whereTabs1',this.srcTabInfoInVo.whereTabs1);
+    if(i==2) localStorage.setItem('whereTabs2',this.srcTabInfoInVo.whereTabs2);
+    if(i==3) localStorage.setItem('whereTabs3',this.srcTabInfoInVo.whereTabs3);
+    if(i==4) localStorage.setItem('whereTabs4',this.srcTabInfoInVo.whereTabs4);
 
-    console.log("this.tabInfoInVo.whereColTab=="+this.tabInfoInVo.whereColTab); 
+    console.log("this.srcTabInfoInVo.whereColTab=="+this.srcTabInfoInVo.whereColTab); 
   }
   /*****************************
    * 컬럼 혹은 컬럼포함 테이블 검색
    ****************************/
   onChangeComboWhereColTab(i: number) {
-    this.tabInfoInVo.whereColTab = this.comboWhereColTab[i].value;
+    this.srcTabInfoInVo.whereColTab = this.comboWhereColTab[i].value;
   }  
 
-  /********************
-   * 순차정렬
-   ********************/
-  onChangeComboOrderBy(i) {
-    this.tabInfoInVo.orderBy = this.comboOrderBy[i].value;
-    this.onSelectEtlTab();
-  }
-
-  /********************
-   * 역순정렬
-   ********************/
-  onChangeComboAscDesc(i) {
-    this.tabInfoInVo.ascDesc = this.comboAscDesc[i].value;
-    this.onSelectEtlTab();
-  }
 
   /********************************************
    * 그리드 전체 선택
    ********************************************/
   onCheckGridAll() {
-    for (var i = 0; i < this.tabInfoOutVoList.length; i++) {
-      this.tabInfoOutVoList[i].chk = this.isCheckAll;
-      console.log("chk["+i+"]="+this.tabInfoOutVoList[i].chk);
+    for (var i = 0; i < this.srcTabInfoOutVoList.length; i++) {
+      this.srcTabInfoOutVoList[i].chk = this.isCheckAll;
+      console.log("chk["+i+"]="+this.srcTabInfoOutVoList[i].chk);
     }
   }
 
-  onCheckGrid(i) {
-    console.log("chk1="+this.tabInfoOutVoList[i].chk);
+  onCheckGrid(i : number) {
+    console.log("chk1="+this.srcTabInfoOutVoList[i].chk);
   }
 
 
@@ -198,22 +189,46 @@ export class EtlTabComponent implements OnInit {
   /********************
    * 테이블정보 조회
    ********************/
-  onSelectEtlTab(){
+  onSelectSrcTabList(){
     //this.tabInfoService.tabInfoInVo = this.tabInfoInVo;
     //this.tabInfoService.onSetLocalStorageTabInfo(this.tabInfoInVo);
     //alert("onSelectEtlTab");
-    if(this.tabInfoInVo.selectedTabs==0) this.tabInfoInVo.whereTabs = "";
-    if(this.tabInfoInVo.selectedTabs==1) this.tabInfoInVo.whereTabs = this.tabInfoInVo.whereTabs1;
-    if(this.tabInfoInVo.selectedTabs==2) this.tabInfoInVo.whereTabs = this.tabInfoInVo.whereTabs2;
-    if(this.tabInfoInVo.selectedTabs==3) this.tabInfoInVo.whereTabs = this.tabInfoInVo.whereTabs3;
-    if(this.tabInfoInVo.selectedTabs==4) this.tabInfoInVo.whereTabs = this.tabInfoInVo.whereTabs4;
+    if(this.srcTabInfoInVo.selectedTabs==0) this.srcTabInfoInVo.whereTabs = "";
+    if(this.srcTabInfoInVo.selectedTabs==1) this.srcTabInfoInVo.whereTabs = this.srcTabInfoInVo.whereTabs1;
+    if(this.srcTabInfoInVo.selectedTabs==2) this.srcTabInfoInVo.whereTabs = this.srcTabInfoInVo.whereTabs2;
+    if(this.srcTabInfoInVo.selectedTabs==3) this.srcTabInfoInVo.whereTabs = this.srcTabInfoInVo.whereTabs3;
+    if(this.srcTabInfoInVo.selectedTabs==4) this.srcTabInfoInVo.whereTabs = this.srcTabInfoInVo.whereTabs4;
   
-    this.tabInfoService.selectTabList(this.tabInfoInVo)
+    this.tabInfoService.selectTabList(this.srcTabInfoInVo)
     .subscribe(result => {
        if(!result.isSuccess) alert(result.errUsrMsg)
       else {
-        this.tabInfoOutVoList = result.tabInfoOutVoList;
-        this.cnt = this.tabInfoOutVoList.length;
+        this.srcTabInfoOutVoList = result.tabInfoOutVoList;
+        this.srcCnt = this.srcTabInfoOutVoList.length;
+        //console.log(result.tabInfoOutVoList);
+        //alert("onSelectMetaTabInfoList");
+      }
+    });
+    
+    this.onSelectTarTabList();
+  }
+
+  onSelectTarTabList(){
+    //this.tabInfoService.tabInfoInVo = this.tabInfoInVo;
+    //this.tabInfoService.onSetLocalStorageTabInfo(this.tabInfoInVo);
+    //alert("onSelectEtlTab");
+    if(this.srcTabInfoInVo.selectedTabs==0) this.tarTabInfoInVo.whereTabs = "";
+    if(this.srcTabInfoInVo.selectedTabs==1) this.tarTabInfoInVo.whereTabs = this.srcTabInfoInVo.whereTabs1;
+    if(this.srcTabInfoInVo.selectedTabs==2) this.tarTabInfoInVo.whereTabs = this.srcTabInfoInVo.whereTabs2;
+    if(this.srcTabInfoInVo.selectedTabs==3) this.tarTabInfoInVo.whereTabs = this.srcTabInfoInVo.whereTabs3;
+    if(this.srcTabInfoInVo.selectedTabs==4) this.tarTabInfoInVo.whereTabs = this.srcTabInfoInVo.whereTabs4;
+  
+    this.tabInfoService.selectTabList(this.tarTabInfoInVo)
+    .subscribe(result => {
+       if(!result.isSuccess) alert(result.errUsrMsg)
+      else {
+        this.tarTabInfoOutVoList = result.tabInfoOutVoList;
+        this.tarCnt = this.tarTabInfoOutVoList.length;
         //console.log(result.tabInfoOutVoList);
         //alert("onSelectMetaTabInfoList");
       }
@@ -221,58 +236,17 @@ export class EtlTabComponent implements OnInit {
   }
 
   /********************
-   * DDL 생성
-   ********************/
-  onDDL() {
-    console.log("onDDL");
-    this.tabInfoService.selectCreateScript(this.tabInfoOutVoList)
-    .subscribe(result => {
-       if(!result.isSuccess) alert(result.errUsrMsg)
-      else {
-        //this.tabInfoOutVoList = result.tabInfoOutVoList;
-        console.log(result.createScript);
-        this.createScript = result.createScript;
-        //alert(this.createScript);
-        //this.router.navigate(["/ext-stat-view/"+this.createScript]);
-        this.router.navigate(['/ext-stat-view',{result: this.createScript}]);
-        //this.router.navigate(['/ext-stat-view/',{debug: true}]);
-       
-      }
-    });
-  }
-
-  /********************
-   * SELECT 쿼리
-   ********************/
-  onSelectSelectScript() {
-//    console.log("onSelectSelectScript");
-//    this.tabInfoService.selectSelectScript(this.tabInfoOutVoList)
-//    .subscribe(result => {
-//       if(!result.isSuccess) alert(result.errUsrMsg)
-//      else {
-//        //this.tabInfoOutVoList = result.tabInfoOutVoList;
-//        console.log(result.sqlScript);
-//        //this.createScript = result.createScript;
-//        //alert(this.createScript);
-//        //this.router.navigate(["/ext-stat-view/"+this.createScript]);
-//        this.router.navigate(['/ext-stat-view',{result: result.sqlScript}]);
-//        //this.router.navigate(['/ext-stat-view/',{debug: true}]);
-//      }
-//    });
-  }
-
-  /********************
    * 테이블건수 현행화 갱신
    ********************/
   onUpdateTabRowsUpdateScript() {
     console.log("onUpdateTabRowsUpdateScript");
-    this.tabInfoService.updateTabRowsUpdateScript(this.tabInfoOutVoList)
+    this.tabInfoService.updateTabRowsUpdateScript(this.srcTabInfoOutVoList)
     .subscribe(result => {
        if(!result.isSuccess) alert(result.errUsrMsg)
       else {
         //this.tabInfoOutVoList = result.tabInfoOutVoList;
         console.log(result.tabRowsUpdateScript);
-        this.tabRowsUpdateScript = result.tabRowsUpdateScript;
+        //this.tabRowsUpdateScript = result.tabRowsUpdateScript;
         //alert(this.createScript);
         //this.router.navigate(["/ext-stat-view/"+this.createScript]);
         //this.router.navigate(['/ext-stat-view',{result: this.createScript}]);
@@ -290,14 +264,14 @@ export class EtlTabComponent implements OnInit {
 
       var tabListExcels = []; 
 
-      for( var i in this.tabInfoOutVoList ) {
+      for( var i in this.srcTabInfoOutVoList ) {
           var excel = new TabListExcel();
-          excel.owner        = this.tabInfoOutVoList[i].owner      ; 
-          excel.tabNm        = this.tabInfoOutVoList[i].tabNm       ; 
-          excel.tabHnm       = this.tabInfoOutVoList[i].tabHnm      ; 
-          excel.tabRows      = this.tabInfoOutVoList[i].tabRows     ; 
-          excel.tabUpdDt2    = this.tabInfoOutVoList[i].tabUpdDt2   ; 
-          excel.tabRegDt2    = this.tabInfoOutVoList[i].tabRegDt2   ;
+          excel.owner        = this.srcTabInfoOutVoList[i].owner      ; 
+          excel.tabNm        = this.srcTabInfoOutVoList[i].tabNm       ; 
+          excel.tabHnm       = this.srcTabInfoOutVoList[i].tabHnm      ; 
+          excel.tabRows      = this.srcTabInfoOutVoList[i].tabRows     ; 
+          excel.tabUpdDt2    = this.srcTabInfoOutVoList[i].tabUpdDt2   ; 
+          excel.tabRegDt2    = this.srcTabInfoOutVoList[i].tabRegDt2   ;
           tabListExcels.push(excel);
       }         
 
